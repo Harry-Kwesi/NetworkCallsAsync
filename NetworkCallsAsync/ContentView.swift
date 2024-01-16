4205c36cace5718b17a5edba298735658c8addb9
Harry-Kwesi <harrydegraft@hotmail.com>
2024-01-16 21:59:28 +0530

    Initial Commit

---
+++ NetworkCallsAsync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkCallsAsync
+//
+//  Created by Harry Kwesi De Graft on 16/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkCallsAsync/NetworkCallsAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallsAsyncApp.swift
+//  NetworkCallsAsync
+//
+//  Created by Harry Kwesi De Graft on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallsAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
